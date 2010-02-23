0452d8c8ffb456393a071543114472a117af6533
Dag Erling Smørgrav <dages@met.no>
2010-02-23 12:48:59 +0000

    style nit

--- CoClient.cc
+++ CoClient.cc
@@ -100,7 +100,6 @@ CoClient::CoClient(QWidget* parent, const char *name, const char *h,
 	{
 		DWORD size = UNLEN + 1;
 		CHAR name[size];
-
 		if (GetUserNameA(name, &size)) {
 			cerr << "GetUserNameA() failed" << endl;
 			userid = "UnknownWin32User";
